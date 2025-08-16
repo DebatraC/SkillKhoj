69ea7adabcc7797d5d76ce2ec03b0cd1a34241be
Debatra Chatterjee <debatrac2026@email.iimcal.ac.in>
2025-08-16 16:04:44 +0530

    Prerender issue fix

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -2,7 +2,23 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
+    path: '/recruiter/job/:jobId/applicants',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: '/login',
     renderMode: RenderMode.Prerender
+  },
+  {
+    path: '/register',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: '/unauthorized',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: '**',
+    renderMode: RenderMode.Server
   }
 ];
